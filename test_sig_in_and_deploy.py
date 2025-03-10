dd75c9e802d4a18187543c2432c35d55cce0244d
madhavkp <madhavkoli2012@gmail.com>
2025-03-10 08:58:35 +0530

    change file name

---
+++ test_sig_in_and_deploy.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")  # Run without GUI
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://kalaloka.com")
+print(driver.title)
+driver.quit()
