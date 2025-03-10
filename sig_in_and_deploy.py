01202ae53d6402cc4ae088eca5f32ea4ddac61bb
madhavkp <148183430+madhavkp@users.noreply.github.com>
2025-03-10 08:16:23 +0530

    Update sig_in_and_deploy.py
    ddd

--- sig_in_and_deploy.py
+++ sig_in_and_deploy.py
@@ -1,23 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")  # Run without GUI
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-class Schedule_Script:
-
-    def launch(self,):
-        service =  Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
-
-        driver.get(url)
-        driver.maximize_window()
-        input("")
-
-url = "https://kalaloka.com"
-
-Schedule_Script.launch(url)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://kalaloka.com")
+print(driver.title)
+driver.quit()
